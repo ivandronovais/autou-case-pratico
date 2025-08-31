5dc126750f5fbe14079e773f7cdee748f7682791
ivandronovais <ivandronovais@gmail.com>
2025-08-31 12:37:08 -0300

    🐛fix(deploy): configure Flask app to use dynamic port binding

--- api/src/app.py
+++ api/src/app.py
@@ -95,4 +95,5 @@ def classificar_arquivo():
     return jsonify({"error": "Tipo de arquivo não permitido"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
